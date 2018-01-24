0ec8c24c83a18cff97d9cec476936bb897c59049
Margaret Chan <margaret.helen.chan@gmail.com>
2018-01-23 19:48:24 -0500

    tues 7:48 pm maggies files

--- foodsquare project/foodsquare/View Controllers/CreateCollectionViewController.swift
+++ foodsquare project/foodsquare/View Controllers/CreateCollectionViewController.swift
@@ -8,28 +8,50 @@
 
 import UIKit
 
-class CreateCollectionViewController: UIViewController {
+/// Presented via Favorites Tab > Top Right Nav Bar Button (+) > Modal View to Create a Favorites Folder/Collection
 
+class CreateCollectionViewController: UIViewController {
+    
+    let createCollectionView = CreateCollectionView()
+    
+    let newCollectionName: String? = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        self.view.backgroundColor = .white
+        self.view.addSubview(createCollectionView)
+        createCollectionView.textField.delegate = self
+        configureNavBar()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    private func configureNavBar() {
+        navigationItem.title = "Create Collection"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        let createBarItem = UIBarButtonItem(title: "Create", style: .plain, target: self, action: #selector(createCollection))
+        navigationItem.rightBarButtonItem = createBarItem
     }
-    */
-
+    
+    @objc func createCollection() {
+        createCollectionView.textField.text = newCollectionName
+        print("Create button pressed")
+        
+        /// save this New Collection Name to the Favorites VC Collection View
+        
+        /// create a new empty Collection of Favorites with the given name from the textfield
+    }
 }
+
+
+extension CreateCollectionViewController: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.becomeFirstResponder()
+    }
+    
+    /// do not use this function below, use createCollection button instead
+    //    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    //        textField.resignFirstResponder()
+    //        return true
+    //    }
+}
+
+
--- foodsquare project/foodsquare/Views/CreateCollectionView.swift
+++ foodsquare project/foodsquare/Views/CreateCollectionView.swift
@@ -7,15 +7,49 @@
 //
 
 import UIKit
+import SnapKit
 
 class CreateCollectionView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    // top right bar button hosted on CreateCollectionViewController
+    
+    lazy var textField: UITextField = {
+        let textfield = UITextField()
+        textfield.placeholder = "Enter a New Collection Title"
+        textfield.borderStyle = .line
+        textfield.textAlignment = .center
+        return textfield
+    }()
+    
+    // setup custom view
+    
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        commonInit()
     }
-    */
-
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        backgroundColor = .clear
+        setupViews()
+    }
+    
+    private func setupViews() {
+        setupTextField()
+    }
+    
+    private func setupTextField() {
+        addSubview(textField)
+        // snapkit constraints
+        textField.snp.makeConstraints { (make) in
+            make.top.equalTo(safeAreaLayoutGuide.snp.top)
+            make.width.equalTo(safeAreaLayoutGuide.snp.width)
+            make.height.equalTo(safeAreaLayoutGuide.snp.height).multipliedBy(0.09)
+            make.centerX.equalTo(center)
+        }
+    }
 }
