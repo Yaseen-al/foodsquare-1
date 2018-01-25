a060bed95c5e47c10dcc307905ecf17d5f317e98
clint-steve-m <clint_m@clintmejia.com>
2018-01-25 17:06:38 -0500

    welcome controller added

---
+++ foodsquare project/foodsquare/Views/WelcomeView.swift
@@ -0,0 +1,13 @@
+//
+//  WelcomeView.swift
+//  foodsquare
+//
+//  Created by Clint Mejia on 1/25/18.
+//  Copyright © 2018 C4Q. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeView: UIView {
+
+}
